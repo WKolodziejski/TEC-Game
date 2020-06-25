8060bbb5d9d62bc2382e410873c4eabb3932b209
Gustavo Rehbein <gustavohubner98@yahoo.com.br>
2020-06-25 17:58:48 -0300

    melhoras e plataformas

--- Unity Project/Assets/Controller.cs
+++ Unity Project/Assets/Controller.cs
@@ -1,21 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-
+    public float speed = 5f;
+    public float jumpForce = 15;
+    public BoxCollider2D groundcheck;
     public Transform tr;
     public Rigidbody2D rb;
-    public float speed = 30f;
-    public float jumpForce = 300f;
     float horizontalMov;
+    float lastJump;
+    public float fallTime = 0.3f;
+    public float jumpLock = 0.6f;
     bool jump;
+
+    PolygonCollider2D playerCollider;
     public bool grounded = false;
+    public bool platform = false;
     void Start()
     {
         tr = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
+        groundcheck = GetComponent<BoxCollider2D>();
+        playerCollider = GetComponent<PolygonCollider2D>();
     }
 
     // Update is called once per frame
@@ -23,27 +32,51 @@ public class Controller : MonoBehaviour
     {
         jump = Input.GetButtonDown("Jump");
 
-        if (jump && grounded){
+        if (jump && grounded && JumpCooldown()){
             rb.AddForce(jumpForce * Vector2.up, ForceMode2D.Impulse);
             jump = false;
+            lastJump = Time.time;
         }
-        horizontalMov = Input.GetAxis("Horizontal");
+        horizontalMov = Input.GetAxis("Horizontal");        
+    }
 
-        
+    private bool JumpCooldown()
+    {
+        return (Time.time - lastJump >= jumpLock);
     }
 
     void FixedUpdate(){
         transform.position += horizontalMov * Time.deltaTime * speed * Vector3.right;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision){
-        if (collision.collider.tag == "Ground"){
+    private void OnTriggerEnter2D(Collider2D collider){
+        if (collider.tag == "Ground"){
+            grounded = true;
+        }
+
+        if (collider.tag == "Platform"){
+            platform = true;
             grounded = true;
         }
     }
-    private void OnCollisionExit2D(Collision2D collision){
-        if (collision.collider.tag == "Ground"){
+
+
+
+    private void OnTriggerExit2D(Collider2D collider){
+        if (collider.tag == "Ground"){
             grounded = false;
         }
+
+        if (collider.tag == "Platform"){
+            platform = false;
+            grounded = false;
+        }
+    }
+    
+    public IEnumerator Fall(){
+        playerCollider.enabled = false;
+        lastJump = Time.time;
+        yield return new WaitForSeconds(fallTime);
+        playerCollider.enabled = true;
     }
 }
---
+++ Unity Project/Assets/Platforms.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platforms : MonoBehaviour
+{
+    public Controller player;
+    public float timeToFall = 0.3f;
+    float waitTime = 0f;
+    
+    PlatformEffector2D effector2D;
+
+    void Start(){
+        effector2D = GetComponent<PlatformEffector2D>();
+    }
+    void Update()
+    {
+        if ((player.platform) && (Input.GetAxis("Vertical") < -0.8f) && (Input.GetAxis("Horizontal") < 0.2f) && (Input.GetAxis("Horizontal") > -0.2f)) {
+           Debug.Log("waittime:"+ waitTime );
+           if (waitTime > timeToFall) {
+               StartCoroutine(player.Fall());
+               waitTime = 0f;
+           } else {
+               waitTime += Time.deltaTime;
+           }
+
+        } 
+    }
+
+
+}
