1e331b966baad8dadc61faa542c87723a68bf31c
William Kolodziejski <william.kol.wk@gmail.com>
2020-06-25 12:10:43 -0300

    Lógica para persistir os objetos ao trocar de cena

---
+++ Unity Project/Assets/Scripts/Hacking/SceneReferences.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneReferences : Singleton<SceneReferences>
+{
+    protected SceneReferences() { }
+
+    public void PersistObjects()
+    {
+        PersistPlayer();
+        //TODO: persistir todas entidades e atributos da cena
+    }
+
+    public void ReloadObjects()
+    {
+        ReloadPlayer();
+    }
+
+    private Vector3 player_position;
+    private Quaternion player_rotation;
+
+    private void PersistPlayer()
+    {
+        Transform player = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+        player_position = player.position;
+        player_rotation = player.rotation;
+
+    }
+
+    private void ReloadPlayer()
+    {
+        Transform player = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+        player.position = player_position;
+        player.rotation = player_rotation;
+    }
+
+}
---
+++ Unity Project/Assets/Scripts/ReturnHack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnHack : MonoBehaviour
+{
+  
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.M))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
