234fa6a262ec0c2c9b440ed14d7792f883d23b14
William Kolodziejski <wkolodziejski@inf.ufpel.edu.br>
2020-07-09 15:32:39 -0300

    Weapon

---
+++ Unity Project/Assets/Scripts/Weapon/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public float damage = 1f;
+    public GameObject hit;
+    public GameObject muzzle;
+
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+
+        Destroy(Instantiate(muzzle, gameObject.transform.position, gameObject.transform.rotation), 0.2f);
+        Destroy(gameObject, 1f);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(Instantiate(hit, gameObject.transform.position, gameObject.transform.rotation), 1f);
+        Destroy(gameObject);
+    }
+
+}
---
+++ Unity Project/Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    
+    public GameObject bullet;
+    public Transform barrel;
+    public float cooldown = 0.2f;
+
+    private float lastCooldown;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            fire();
+        }
+    }
+
+    void fire()
+    {
+        if (lastCooldown <= Time.time)
+        {
+            lastCooldown = Time.time + cooldown;
+            Instantiate(bullet, barrel.position, barrel.rotation);
+        }
+    }
+
+}
