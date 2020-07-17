8a26471224668fc1086730df07b60f01cf618aa2
gustavohubner <34004020+gustavohubner@users.noreply.github.com>
2020-07-17 18:30:32 -0300

    menu principal

---
+++ Unity Project/Assets/MainMenuButtons.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void NewGame(){
+        SceneManager.LoadScene("Level1",LoadSceneMode.Single);
+    }
+    public void QuitGame(){
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
