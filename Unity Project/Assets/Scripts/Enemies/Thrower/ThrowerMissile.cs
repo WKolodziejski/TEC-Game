4e1bc3caefe20c6cbad49c202b9ffbda87e5ebc0
William Kolodziejski <wkolodziejski@inf.ufpel.edu.br>
2020-07-21 01:17:37 -0300

    missile

---
+++ Unity Project/Assets/Scripts/Enemies/Thrower/ThrowerMissile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowerMissile : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(Vector3.right * 100f);
+    }
+
+    
+    void Update()
+    {
+        if (transform.rotation.eulerAngles.z > -90)
+            transform.rotation *= Quaternion.Euler(0, 0, -10 * Time.deltaTime);
+    }
+
+}
