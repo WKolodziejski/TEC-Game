2602f7e70a7545b16455ea1e6aec67770682cc80
gustavohubner <34004020+gustavohubner@users.noreply.github.com>
2020-07-20 22:11:20 -0300

    level 2

--- Unity Project/Assets/Scripts/Objects/Crane_Controller.cs
+++ Unity Project/Assets/Scripts/Objects/Crane_Controller.cs
@@ -14,49 +14,62 @@ public class Crane_Controller : MonoBehaviour
     public Transform HAxis;
     public Transform VAxis;
     public Transform wire;
+    public bool startMovOnContact = false;
     private float targetH;
     private float targetV;
+    private bool unlocked;
+
+
+
 
     void Start(){
-       
+       HAxis.localPosition = new Vector3(X, HAxis.localPosition.y, HAxis.localPosition.z);
+       wire.localPosition = new Vector3(wire.localPosition.x, 1 + VAxis.localPosition.y/2, wire.localPosition.z);
+       wire.localScale = new Vector3(wire.localScale.x,-VAxis.localPosition.y + 0.05f, wire.localScale.z);
+       VAxis.localPosition = new Vector3(VAxis.localPosition.x, Y, VAxis.localPosition.z);
+
     }
 
     void FixedUpdate()
     {
-        if (HAxis.localPosition.x <= targetH){
-            X = X + Time.deltaTime * speedH;
-            targetH = rangeH;
-        }
-        else {
-            X = X - Time.deltaTime * speedH;
-            targetH = 0f;
-        }
-        
-        HAxis.localPosition = new Vector3(X, HAxis.localPosition.y, HAxis.localPosition.z);
+        if(unlocked || !startMovOnContact){
+            if (HAxis.localPosition.x <= targetH){
+                X = X + Time.deltaTime * speedH;
+                targetH = rangeH;
+            }
+            else {
+                X = X - Time.deltaTime * speedH;
+                targetH = 0f;
+            }
+            
+            HAxis.localPosition = new Vector3(X, HAxis.localPosition.y, HAxis.localPosition.z);
 
-        if (VAxis.localPosition.y >= targetV){
-            Y = Y - Time.deltaTime * speedV;
-            targetV = -rangeV;
+            if (VAxis.localPosition.y >= targetV){
+                Y = Y - Time.deltaTime * speedV;
+                targetV = -rangeV;
+            }
+            else {
+                Y = Y + Time.deltaTime * speedV;
+                targetV = 0f;
+            }
+            
+            wire.localPosition = new Vector3(wire.localPosition.x, 1 + VAxis.localPosition.y/2, wire.localPosition.z);
+            wire.localScale = new Vector3(wire.localScale.x,-VAxis.localPosition.y + 0.05f, wire.localScale.z);
+            VAxis.localPosition = new Vector3(VAxis.localPosition.x, Y, VAxis.localPosition.z);
         }
-        else {
-            Y = Y + Time.deltaTime * speedV;
-            targetV = 0f;
-        }
-        
-        wire.localPosition = new Vector3(wire.localPosition.x, 1 + VAxis.localPosition.y/2, wire.localPosition.z);
-        wire.localScale = new Vector3(wire.localScale.x,-VAxis.localPosition.y + 0.05f, wire.localScale.z);
-        VAxis.localPosition = new Vector3(VAxis.localPosition.x, Y, VAxis.localPosition.z);
     }
 
     private void OnTriggerStay2D(Collider2D collider){
         if ((collider.tag == "Player")||(collider.tag == "Enemy")){
             collider.transform.SetParent(VAxis);
+            unlocked = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collider){
         if ((collider.tag == "Player")||(collider.tag == "Enemy")){
             collider.transform.SetParent(null);
+            //unlocked = false;
         }
     }
 }
