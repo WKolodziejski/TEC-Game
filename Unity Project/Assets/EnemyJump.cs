57df7988c3582e2f99dfbc1fc3fc3eac49c81c56
gustavohubner <34004020+gustavohubner@users.noreply.github.com>
2020-07-29 18:23:58 -0300

    pulo dos soldados
     com certeza da pra melhorar

---
+++ Unity Project/Assets/EnemyJump.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyJump : MonoBehaviour
+{
+    
+    public Rigidbody2D rb;
+    public BoxCollider2D groundCheck;
+    public jumpcheck jCheck;
+    public float jumpLock = 0.6f;
+    public float jumpForce = 15f;
+    private float lastJump;
+    private bool grounded;
+
+    void start()
+    {
+       /* rb = GetComponent<Rigidbody2D>();
+        jCheck = GetComponent<jumpcheck>();*/
+    }
+
+    void FixedUpdate(){
+        Jump();
+    }
+    void Jump()
+    {
+        if (grounded && JumpCooldown() && !jCheck.ground)
+        {
+            rb.AddForce(jumpForce * Vector2.up, ForceMode2D.Impulse);
+            lastJump = Time.time;
+        }
+    }
+
+    private bool JumpCooldown()
+    {
+        return (Time.time - lastJump >= jumpLock);
+    }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        if ((collider.tag == "Ground") || (collider.tag == "Platform"))
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if ((collider.tag == "Ground") || (collider.tag == "Platform"))
+        {
+            grounded = false;
+        }
+    }
+}
---
+++ Unity Project/Assets/jumpcheck.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpcheck : MonoBehaviour
+{
+    public bool ground;
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        if ((collider.tag == "Ground") || (collider.tag == "Platform"))
+        {
+            ground = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if ((collider.tag == "Ground") || (collider.tag == "Platform"))
+        {
+            ground = false;
+        }
+    }
+
+}
