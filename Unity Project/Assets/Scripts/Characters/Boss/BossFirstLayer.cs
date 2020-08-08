362922565f279fed267c73d9c69ac00cdeb1787b
EmersonPL <emersonphilopes@gmail.com>
2020-08-08 19:32:59 -0300

    Boss layer1 (teste)

--- Unity Project/Assets/Scripts/Characters/Boss/BossFirstLayer.cs
+++ Unity Project/Assets/Scripts/Characters/Boss/BossFirstLayer.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss : MonoBehaviour
+public class BossFirstLayer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -15,9 +15,4 @@ public class Boss : MonoBehaviour
     {
         
     }
-
-    /*protected override void InitializeComponents()
-    {
-        enabled = true;
-    }*/
 }
---
+++ Unity Project/Assets/Scripts/Characters/Boss/BossTurret.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTurret : MonoBehaviour
+{
+    private const int MAX_TURN = 45;
+    public Bullet bullet;
+    private Character target;
+    private float startAngle;
+    private float angle;
+    public float fireRate = 0.5f;
+    private float lastCooldown = 2;
+
+    private Transform firePoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startAngle = transform.eulerAngles.z;
+
+        GetTarget();
+
+        firePoint = transform.Find("FirePoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) GetTarget();
+        angle = GetAngle();
+        SetAngle(angle);
+        Shoot();
+    }
+
+    private void GetTarget()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Character>();
+    } 
+
+    private float GetAngle()
+    {
+        float ang;
+        
+        ang = Vector2.SignedAngle(target.transform.position, transform.position);
+
+        if (ang > - MAX_TURN && ang < MAX_TURN) 
+            return ang;
+
+        return MAX_TURN;
+    }
+
+    private void SetAngle(float ang)
+    {
+        //float ang;
+
+        //ang = Vector2.SignedAngle(target.transform.position, transform.position);
+
+        /*if (ang > - MAX_TURN && ang < MAX_TURN)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, startAngle - ang);
+        }*/
+
+        if (ang != MAX_TURN)
+            transform.rotation = Quaternion.Euler(0, 0, startAngle - ang);
+    }
+
+    public void Shoot() {
+        if (angle == MAX_TURN) return;
+
+        if (lastCooldown > Time.time) return;
+        
+        lastCooldown = Time.time + (1f / fireRate);
+
+        Bullet b = Instantiate(bullet, firePoint.position, firePoint.rotation);
+        b.Fire(0);
+    }
+}
