13e1832aa98fb4519a5dafb9f9c35cdd01852090
William Kolodziejski <wkolodziejski@inf.ufpel.edu.br>
2020-08-10 22:55:52 -0300

    RATO

---
+++ Unity Project/Assets/Scripts/Characters/Rat.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rat : Enemy2D
+{
+
+    protected override void InitializeComponents()
+    {
+        Destroy(gameObject, 20f);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += Vector3.right * movementSpeed * Time.deltaTime * -1f;
+    }
+
+    protected override void OnDie()
+    {
+        base.OnDie();
+
+        Destroy(gameObject);
+    }
+
+    public override void Attack()
+    {
+        
+    }
+
+}
