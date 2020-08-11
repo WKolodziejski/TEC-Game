e64f901c9d6bf6950fe47b1e43c285002205c290
William Kolodziejski <wkolodziejski@inf.ufpel.edu.br>
2020-08-11 19:08:41 -0300

    RATO e luzes

--- Unity Project/Assets/Scripts/CameraRange.cs
+++ Unity Project/Assets/Scripts/CameraRange.cs
@@ -7,18 +7,17 @@ public class CameraRange : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Enemy"))
+        if (collision.CompareTag("Enemy") || collision.CompareTag("Rat"))
             collision.gameObject.GetComponent<Character>().SetEnabled(true);
     }
 
     void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag("Enemy"))
+        if (collision.CompareTag("Enemy") || collision.CompareTag("Rat"))
             collision.gameObject.GetComponent<Character>().SetEnabled(true);
         if (collision.CompareTag("Spawner"))
             collision.gameObject.GetComponent<EnemySpawnerPoint>().SetEnabled(true);
     }
-
 
     void OnTriggerExit2D(Collider2D collision)
     {
@@ -28,7 +27,7 @@ public class CameraRange : MonoBehaviour
         if (collision.CompareTag("Spawner"))
             collision.gameObject.GetComponent<EnemySpawnerPoint>().SetEnabled(false);
 
-        if (collision.tag.Contains("Bullet"))
+        if (collision.tag.Contains("Bullet") || collision.CompareTag("Rat"))
             Destroy(collision.gameObject);
     }
 }
