078d5f52ae55ca529281e76d9d5b63789bf6609b
William Kolodziejski <wkolodziejski@inf.ufpel.edu.br>
2020-08-14 15:42:21 -0300

    Pixel snap + ajustes

--- Unity Project/Assets/Scripts/GameController.cs
+++ Unity Project/Assets/Scripts/GameController.cs
@@ -121,7 +121,7 @@ public class GameController : MonoBehaviour
             /*cam.GetCinemachineComponent<CinemachineFramingTransposer>().m_XDamping = 0;
             cam.Follow = null;
             cam.LookAt = null;*/
-            
+                        
             lifes--;
 
             if (lifes == 0)
@@ -141,7 +141,7 @@ public class GameController : MonoBehaviour
 
     private IEnumerator INewPlayer()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(2.5f);
 
         SetupPlayer();
     }
