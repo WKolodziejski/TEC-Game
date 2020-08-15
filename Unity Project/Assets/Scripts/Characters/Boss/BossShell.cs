493f3012f805ae1b778653405bc165a2bf1375fd
William Kolodziejski <wkolodziejski@inf.ufpel.edu.br>
2020-08-14 21:21:28 -0300

    shell

---
+++ Unity Project/Assets/Scripts/Characters/Boss/BossShell.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossShell : MonoBehaviour
+{
+
+    public Transform insideLeft;
+    public Transform insideRight;
+    public Transform outsideLeft;
+    public Transform outsideRight;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+            Open();
+    }
+
+    public void Open()
+    {
+        StartCoroutine(IOpen());
+    }
+
+    private IEnumerator IOpen()
+    {
+        while (insideLeft.rotation.eulerAngles.z < 90)
+        {
+            insideLeft.Rotate(Vector3.forward, 0.5f);
+            insideRight.Rotate(Vector3.forward, -0.5f);
+
+            if (insideLeft.rotation.eulerAngles.z > 45)
+            {
+                outsideLeft.Rotate(Vector3.forward, 0.5f);
+                outsideRight.Rotate(Vector3.forward, -0.5f);
+            }
+
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+}
