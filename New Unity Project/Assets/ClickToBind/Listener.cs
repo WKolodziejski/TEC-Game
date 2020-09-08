278f3a4f7306dcea3109ecd511b85d848c6f90ce
gustavohubner <34004020+gustavohubner@users.noreply.github.com>
2020-09-08 11:54:14 -0300

    import

---
+++ New Unity Project/Assets/ClickToBind/Listener.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script is included for the sake of the demo scene 
+/// and is not required to use Click to Bind - feel free to delete
+/// </summary>
+public class Listener : MonoBehaviour {
+
+	//Script used as a example of how to use keyBindingManager
+
+	public GameObject box1;
+	public GameObject box2;
+	public GameObject box3;
+	public GameObject box4;
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(KeyBindingManager.GetKeyDown(KeyAction.jump))
+		{
+			box1.SetActive(!box1.activeSelf);
+		}
+
+	}
+}
---
+++ New Unity Project/Assets/Controller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Rigidbody rb;
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump")){
+            rb.AddForce(300*Vector3.up);
+        }
+    }
+}
